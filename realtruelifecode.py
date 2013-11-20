554706afd7cb8d360097502d61437328e4cbf8ce
TheoKeeping <theorowankeeping@gmail.com>
2013-11-20 13:08:43 +0000

    Update realtruelifecode.py

--- realtruelifecode.py
+++ realtruelifecode.py
@@ -1,6 +1,8 @@
-print("Enter the variable you wish to determine:")
 print("Horizontal range (= Sh)")
 print("Velocity (= v)")
 print("Angle (= a)")
 print("Vertical displacement (= Sv)")
 print("Time (=t)")
+variable = input("Enter the variable you wish to determine:")
+if variable != "Sh" and variable != "v" and variable != "a" and variable != "Sv" and variable != "t":
+    print("Please enter a VALID variable... *sighs*")
