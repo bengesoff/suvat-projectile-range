2d716d6a8cbda1919aa42fec374f728e3ac71c99
TheoKeeping <theorowankeeping@gmail.com>
2013-11-20 13:11:35 +0000

    Update realtruelifecode.py

--- realtruelifecode.py
+++ realtruelifecode.py
@@ -1,3 +1,4 @@
+import sys
 print("Horizontal range (= Sh)")
 print("Velocity (= v)")
 print("Angle (= a)")
@@ -6,3 +7,7 @@ print("Time (=t)")
 variable = input("Enter the variable you wish to determine:")
 if variable != "Sh" and variable != "v" and variable != "a" and variable != "Sv" and variable != "t":
     print("Please enter a VALID variable... *sighs*")
+if variable != "Sh" and variable != "v" and variable != "a" and variable != "Sv" and variable != "t":
+    print("This is the final time I'm warning you, VALID variable")
+if variable != "Sh" and variable != "v" and variable != "a" and variable != "Sv" and variable != "t":
+    sys.exit()
