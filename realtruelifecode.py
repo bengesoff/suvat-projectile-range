d15adb6cf32e2deb3c047dc0cd6fa6af21c5493d
TheoKeeping <theorowankeeping@gmail.com>
2013-11-21 14:53:14 +0000

    Update realtruelifecode.py

--- realtruelifecode.py
+++ realtruelifecode.py
@@ -12,27 +12,29 @@ a2 = ("v", "t", "Sv", "th")
 t1 = ("v", "th", "a", "Sv")
 t2 = ("v", "th", "a", "Sh")
 
-# Print greeting and valid inputs.
-print("Horizontal range (= Sh)")
-print("Velocity (= v)")
-print("Angle (= a)")
-print("Vertical displacement (= Sv)")
-print("Time (= t)")
-# Tuple of sarkey comments to display when user doesn't enter a valid variable
-sarkey = ("Please enter a valid variable", "Please enter a VALID variable... *sighs*", "This is the final time I'm warning you, a VALID variable please...")
-i = 0
-variable = ""
-# Loop 4 times
-while i < 4:
-    variable = input("Enter the variable you wish to determine:")
-    if variable != "Sh" and variable != "v" and variable != "a" and variable != "Sv" and variable != "t":
-        if i == 3:
-            # Exit program
-            sys.exit()
-        # Print sarkiness
-        print(sarkey[i])
-    else:
-        # If valid variable is entered, stop asking
-        break
-    i = i + 1
-print("Good choice, I like " + variable + " too!")
+def askinput():   
+    # Print greeting and valid inputs.
+    print("Horizontal range (= Sh)")
+    print("Velocity (= v)")
+    print("Angle (= a)")
+    print("Vertical displacement (= Sv)")
+    print("Time (= t)")
+    # Tuple of sarkey comments to display when user doesn't enter a valid variable
+    sarkey = ("Please enter a valid variable", "Please enter a VALID variable... *sighs*", "This is the final time I'm warning you, a VALID variable please...")
+    i = 0
+    variable = ""
+    # Loop 4 times
+    while i < 4:
+        variable = input("Enter the variable you wish to determine:")
+        if variable != "Sh" and variable != "v" and variable != "a" and variable != "Sv" and variable != "t":
+            if i == 3:
+                # Exit program
+                sys.exit()
+            # Print sarkiness
+            print(sarkey[i])
+        else:
+            # If valid variable is entered, stop asking
+            break
+        i = i + 1
+    print("Good choice, I like " + variable + " too!")
+variable = askinput()
