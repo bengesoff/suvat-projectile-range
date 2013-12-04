99fd7b96a734452057098256695073f5052e2ddd
bengesoff <ben.gesoff@gmail.com>
2013-12-04 13:19:36 +0000

    Debugged
    current variables are Sh and Sv

--- realtruelifecode.py
+++ realtruelifecode.py
@@ -13,7 +13,7 @@ a2 = ("v", "t", "Sv", "th")
 t1 = ("v", "th", "a", "Sv")
 t2 = ("v", "th", "a", "Sh")
 
-def ask_initial_input():   
+def ask_initial_input():
     # Print greeting and valid inputs.
     print("Horizontal range (= Sh)")
     print("Velocity (= v)")
@@ -38,6 +38,7 @@ def ask_initial_input():
             break
         i = i + 1
     print("Good choice, I like " + variable + " too!")
+    return variable
   
 def velocity():
     # function for finding the projectile's velocity
@@ -48,42 +49,42 @@ def velocity():
             # Constant for average accleration due to gravity
             acceleration = 9.80665
             # Ask user for required variables
-            time = input("Please enter the time for which your projectile is in the air")
-            angle = input("Please enter the angle at which your projectile is launched")
-            horizontal_range = input("Please enter the range your projectile travelled, for which I shall circumstantially be very upset if you do not know it")
+            time = float(input("Please enter the time for which your projectile is in the air"))
+            angle = float(input("Please enter the angle at which your projectile is launched"))
+            horizontal_range = float(input("Please enter the range your projectile travelled, for which I shall circumstantially be very upset if you do not know it"))
             return (0.5 * acceleration * (time ** 2) + horizontal_range) / (math.cos(angle.radians()) * time)
             break
-        else if i == "v":
+        elif i == "v":
             # Constant for average accleration due to gravity
             acceleration = 9.80665
             # Ask user for required variables
-            time = input("Please enter the time for which your projectile is in the air")
-            angle = input("Please enter the angle at which your projectile is launched")
-            vertical_displacement = input("Please enter the vertical displacement that your projectile travelled, for which I shall circumstantially be very upset if you do not know it")
+            time = float(input("Please enter the time for which your projectile is in the air"))
+            angle = float(input("Please enter the angle at which your projectile is launched"))
+            vertical_displacement = float(input("Please enter the vertical displacement that your projectile travelled, for which I shall circumstantially be very upset if you do not know it"))
             return (0.5 * acceleration * (time ** 2) + vertical_displacement) / (math.sin(angle.radians()) * time)
             break
         else:
             print("No you silly... enter v or h, otherwise this message will monotonously repeat itslef until you desist")
 def horizontal_range():
     # Ask user for required variables
-    velocity = input("Please enter the initial velocity of your projectile. [m/s]")
-    angle = input("Please enter the angle at which your projectile is being fired. [degrees]")
-    time = input("Please enter the time that your projectile will be in the air for. [seconds]")
+    velocity = float(input("Please enter the initial velocity of your projectile. [m/s]"))
+    angle = float(input("Please enter the angle at which your projectile is being fired. [degrees]"))
+    time = float(input("Please enter the time that your projectile will be in the air for. [seconds]"))
     # Calculate v.cos(th).t
     return velocity * math.cos(math.radians(angle)) * time
     
 def vertical_displacement():
     # Ask user for required variables
     acceleration = 9.80665
-    velocity = input("Please enter the initial velocity of your projectile. [m/s]")
-    angle = input("Please enter the angle at which your projectile is being fired. [degrees]")
-    time = input("Please enter the time that your projectile will be in the air for. [seconds]")
+    velocity = float(input("Please enter the initial velocity of your projectile. [m/s]"))
+    angle = float(input("Please enter the angle at which your projectile is being fired. [degrees]"))
+    time = float(input("Please enter the time that your projectile will be in the air for. [seconds]"))
     # Calculate v.sin(th).t + 0.5.a.t^2
     return (velocity * math.sin(math.radians(angle)) * time) + (0.5 * acceleration * (time ** 2))
 
 
 variable = ask_initial_input()
 if variable == "Sh":
-    print("The horizontal range of your projectile will be: " + horizontal_range() + "m")
+    print("The horizontal range of your projectile will be: " + str(round(horizontal_range(), 3)) + "m")
 if variable == "Sv":
-    print("The vertical displacement of your projectile will be: " + vertical_displacement() + "m")
+    print("The vertical displacement of your projectile will be: " + str(round(vertical_displacement(), 3)) + "m")
